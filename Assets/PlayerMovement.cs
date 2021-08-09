c766b258bb8085a8a2f27b445c8f3572cc4c5350
LAPTOP-HUGO\hihug <hi.hugo.cornu@hotmail.com>
2021-08-09 13:56:38 +0200

    Mouvements

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+	public float moveSpeed = 5f;
+
+	private Rigidbody2D rg;
+
+	Vector2 movement;
+
+	private void Start()
+	{
+		rg = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () 
+	{
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+	}
+
+	void FixedUpdate()
+	{
+		rg.MovePosition(rg.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+}
