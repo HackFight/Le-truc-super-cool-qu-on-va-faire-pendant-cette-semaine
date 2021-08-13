20b1c23dd51bac8230bb4a87d3abec75a859653d
Mistergreen <Thewhiteshadow664@gmail.com>
2021-08-13 15:22:31 +0200

    J'avais pas précisé au pistolet de faire un son, smh smh

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -320,6 +320,7 @@ public class PlayerMovement : MonoBehaviour
 		if (canShoot)
 		{
 			canShoot = false;
+			soundManager.PlayShoot();
 			GameObject temporaryBullet = Instantiate(bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
 			temporaryBullet.GetComponent<Rigidbody>().AddForce((bulletSpawnPoint.position - transform.position) * bulletSpeed);
 			Invoke("SetCanShootToTrue", shootCooldown);
